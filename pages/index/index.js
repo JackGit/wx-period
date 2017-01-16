81a218b9a5bac5fd01d36f66495e34c5894a9f13
Jack Yang <ygjack414@hotmail.com>
2017-01-16 23:31:39 +0800

    add canvas test

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,22 @@
 var app = getApp()
 Page({
+  onReady () {
+    this.drawRect()
+  },
   tapMore () {
     wx.navigateTo({url: '../month/month'})
   },
   tapAdd () {
     wx.navigateTo({url: '../edit/edit'})
+  },
+  drawRect () {
+    const ctx = wx.createCanvasContext('myCanvas')
+    const grd = ctx.createLinearGradient(0, 0, 200, 0)
+    grd.addColorStop(0, 'red')
+    grd.addColorStop(1, 'white')
+    ctx.rect(10, 10, 150, 75)
+    ctx.setFillStyle(grd)
+    ctx.fill()
+    ctx.draw()
   }
 })
